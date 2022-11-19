298cdc04fc9ae4329ca6da57c3b806db9578ae85
Vebjørn <vebjorn@klang.no>
2022-11-19 18:58:04 +0100

    reknar ut relativ feil i 9

--- kode/prob_9.py
+++ kode/prob_9.py
@@ -38,6 +38,23 @@ for i in range(4):
 L = np.array([40, 60, 80, 100])
 # """Plot"""
 regress = st.linregress(1/L, Tc)
-
+Tc_inf = regress[1]
+Tc_analytic = 2/(np.log(1 + np.sqrt(2)))
+#print
+relerr = abs((Tc_analytic - Tc_inf)/Tc_analytic)
+print(f"Avereged Tc's: {Tc}")
 print(f"a-value: {regress[0]}")
-print(f"Tc-estimate: {regress[1]}")
+print(f"Tc-estimate: {Tc_inf}")
+print(f"Analytic Tc: {Tc_analytic}")
+print(f"Relative error: {relerr}")
+
+x_min, x_max = -0.005, 0.03
+x = np.linspace(x_min, x_max)
+y = regress[0]*x + Tc_inf
+
+ # fig = plt.figure()
+# ax = plt.axes()
+# ax.plot(1/L, Tc, 'x', color = 'r')
+# ax.plot(x, y, '--', color  = 'k')
+# ax.axhline(Tc_inf, color = 'k')
+# plt.show()
